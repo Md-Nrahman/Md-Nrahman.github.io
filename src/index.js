2938f969dc4f40a32d5e45bda300d59fb3cff3fa
Naimur Rahman <naimur.rahman@suffixit.com>
2023-11-28 17:30:21 +0600

    Brushup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Header from "./screens/Header";
 import Introduction from "./screens/Introduction";
 import Skills from "./screens/Skills";
 import Experience from "./screens/Experience";
+import Projects from "./screens/Projects";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,6 +15,7 @@ root.render(
     <Introduction />
     <Skills />
     <Experience />
+    <Projects />
   </React.StrictMode>
 );
 
---
+++ src/screens/Projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <section className="lg:px-32 md:px-14 px-7 flex flex-col justify-center py-14 bg-white min-h-screen">
+      <div className=" flex flex-col items-center w-5/6 lg:w-3/6 mx-auto space-y-6 ">
+        <h1 className="font-bold text-5xl">
+          <span className="text-[#66bb6a]">&#60;</span>Projects
+          <span className="text-[#66bb6a]">&#47;&#62;</span>
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
